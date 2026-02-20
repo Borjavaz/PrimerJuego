acb4df17e8699352faed5f15ccf5f4a442029f54
Borja <bdesaavazquez@danielcastelao.org>
2026-02-20 10:25:32 +0100

    actualizacion

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+//movimiento de los cubos
 public class Rotator : MonoBehaviour
 {
-    
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-
     }
+ 
 }
